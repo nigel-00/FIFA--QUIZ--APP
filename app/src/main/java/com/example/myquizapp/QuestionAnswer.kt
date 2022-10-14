520ac7913e7ee56e810b984071fc2fecbc05eeec
Melokuhle Nigel Dube <101907993+nigel-00@users.noreply.github.com>
2022-10-14 17:50:48 -0600

    Initial commit

---
+++ app/src/main/java/com/example/myquizapp/QuestionAnswer.kt
@@ -0,0 +1,41 @@
+package com.example.myquizapp
+
+object QuestionAnswer {
+    var question = arrayOf(
+        "For which Brazilian league team did Pele play for 19 seasons?",
+        "Which country won the first ever FIFA Women’s World Cup in 1991?",
+        "Which former A.C. Milan star became president of Liberia in 2018?",
+        "Boca Juniors and River Plate are both professional soccer teams in which city?",
+        " How many times did Italy win the FIFA world cup?",
+        "Which player scored more goals in the FIFA world cup?",
+        "How many football teams participated in the first football world cup?",
+        "Who won the first football world cup?",
+        "Which team reached in final 3 times but not won the world cup?",
+        "In which country first FIFA world cup was held and in which year?"
+    )
+    var choices = arrayOf(
+        arrayOf("Santos", "Flamengo", "Newell Old Boys", "New York Cosmos"),
+        arrayOf( "Nigeria", "Japan", "Brazil","USA"),
+        arrayOf("Bob Marley","George Weah","Kwame Krumah", "Robert Mugabe"),
+        arrayOf( "Brasilia", "Montevideo","Buenos Aries", "Texas"),
+        arrayOf("7 times", "3 times", "4 times", "8 times"),
+        arrayOf("Pele", "Cristiano Ronaldo", "Lionel Messi", "Muraslov Klose"),
+        arrayOf("24", "13", "20", "10"),
+        arrayOf("USA", "Italy", "Germany", "Uruguay"),
+        arrayOf("France", "Argentina", "Holland", "Wales"),
+        arrayOf("Brazil", "Tanzania", "Greece", "Uruguay")
+
+    )
+    var correctAnswers = arrayOf(
+        "Santos",
+        "USA",
+        "George Weah",
+        "Buenos Aries",
+        "4 times",
+        "Muraslov Klose",
+        "13",
+        "Uruguay",
+        "Holland",
+        "Uruguay"
+    )
+}
